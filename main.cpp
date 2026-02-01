e52cd26f821a46ff3ef00517a61068fa9d5f0cca
muresaijaideepreddy <saijaideepreddymure@tamu.edu>
2026-02-01 12:06:02 -0600

    Created main method to test code for that

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "lidar_preprocessing.hpp"
+#include "pointcloud_loader.hpp"
+#include <iostream>
+int main()
+{
+    std::string filename = "lidar_data.txt";
+    std::vector<PointsXYZ> raw_points = loadPointCloudXYZ(filename);
+    float min_range = 1.0f;
+    float max_range = 50.0f;
+    float min_height = -2.0f;
+    float max_height = 2.0f;
+    std::vector<PointsXYZ> filtered_points = preprocessLidarData(raw_points, min_range, max_range, min_height, max_height);
+    std::cout <<"Filtred Points of Size:"<< filtered_points.size() << std::endl;
+    return 0;
+}
